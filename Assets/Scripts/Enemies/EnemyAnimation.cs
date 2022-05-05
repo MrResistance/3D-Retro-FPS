63cabb185da694872778b8ec0320000c0a81bcbc
MrResistance <Paul1208Gush@Gmail.com>
2022-05-05 19:34:03 +0100

    Enemy animation beginnings

--- Assets/Scripts/Enemies/EnemyAnimation.cs
+++ Assets/Scripts/Enemies/EnemyAnimation.cs
@@ -6,20 +6,21 @@ public class EnemyAnimation : MonoBehaviour
 {
     // The target marker.
     public Transform target;
-
+    public Vector3 targetDirection;
+    private Animator anim;
     // Angular speed in radians per sec.
     public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Determine which direction to rotate towards
-        Vector3 targetDirection = target.position - transform.position;
+        targetDirection = target.position - transform.position;
         targetDirection.y = 0;
         // The step size is equal to speed times frame time.
         float singleStep = speed * Time.deltaTime;
@@ -32,5 +33,7 @@ public class EnemyAnimation : MonoBehaviour
 
         // Calculate a rotation a step closer to the target and applies rotation to this object
         transform.rotation = Quaternion.LookRotation(newDirection);
+        anim.SetFloat("PlayerX", targetDirection.x);
+        anim.SetFloat("PlayerZ", targetDirection.z);
     }
 }
