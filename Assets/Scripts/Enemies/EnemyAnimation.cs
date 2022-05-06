3a5339f6aa631f78b99af7ae5eacaf94a4ab9f35
MrResistance <Paul1208Gush@Gmail.com>
2022-05-06 16:48:35 +0100

    Enemy animation is correct when circling them

--- Assets/Scripts/Enemies/EnemyAnimation.cs
+++ Assets/Scripts/Enemies/EnemyAnimation.cs
@@ -7,33 +7,60 @@ public class EnemyAnimation : MonoBehaviour
     // The target marker.
     public Transform target;
     public Vector3 targetDirection;
+    public bool MirrorLeft = true;
     private Animator anim;
+    private SpriteRenderer sr;
+    public Camera MainCamera;
+    public int directions = 8;
+    float minMirrorAngle = 0;
+    float maxMirrorAngle = 0;
     // Angular speed in radians per sec.
     public float speed = 1.0f;
-    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (MainCamera == null)
+        {
+            MainCamera = Camera.main;
+        }
+    }
     void Start()
     {
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        if (directions <= 0)
+        {
+            directions = 1;
+        }
+        minMirrorAngle = (360 / directions) / 2;
+        maxMirrorAngle = 180 - minMirrorAngle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Determine which direction to rotate towards
-        targetDirection = target.position - transform.position;
-        targetDirection.y = 0;
-        // The step size is equal to speed times frame time.
-        float singleStep = speed * Time.deltaTime;
+        //// Determine which direction to rotate towards
+        //targetDirection = target.position - transform.position;
+        //targetDirection.y = 0;
+        //// The step size is equal to speed times frame time.
+        //float singleStep = speed * Time.deltaTime;
 
-        // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        //// Rotate the forward vector towards the target direction by one step
+        //Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
 
-        // Draw a ray pointing at our target in
-        Debug.DrawRay(transform.position, newDirection, Color.red);
+        //// Draw a ray pointing at our target in
+        //Debug.DrawRay(transform.position, newDirection, Color.red);
 
-        // Calculate a rotation a step closer to the target and applies rotation to this object
-        transform.rotation = Quaternion.LookRotation(newDirection);
-        anim.SetFloat("PlayerX", targetDirection.x);
-        anim.SetFloat("PlayerZ", targetDirection.z);
+        //// Calculate a rotation a step closer to the target and applies rotation to this object
+        //transform.rotation = Quaternion.LookRotation(newDirection);
+        //anim.SetFloat("PlayerX", targetDirection.x);
+        //anim.SetFloat("PlayerZ", -targetDirection.z);
+
+        Vector3 viewDirection = -new Vector3(MainCamera.transform.forward.x, 0, MainCamera.transform.forward.z);
+        transform.LookAt(transform.position + viewDirection);
+        anim.SetFloat("ViewAngle", transform.localEulerAngles.y);
+        if (MirrorLeft)
+        {
+            sr.flipX = !(transform.localEulerAngles.y >= minMirrorAngle && transform.localEulerAngles.y <= maxMirrorAngle);
+        }
     }
 }
