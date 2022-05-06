a9acca132fda0c8ce767a0e76163025202e6afa5
MrResistance <Paul1208Gush@Gmail.com>
2022-05-06 17:55:11 +0100

    Started work on state machine for enemies

--- Assets/Scripts/Enemies/EnemyStateMachine.cs
+++ Assets/Scripts/Enemies/EnemyStateMachine.cs
@@ -8,23 +8,36 @@ public class EnemyStateMachine : MonoBehaviour
     private NavMeshAgent agent;
     private SphereCollider sc;
     public Transform playerObj;
+    public state State = state.idle;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         sc = GetComponent<SphereCollider>();
         playerObj = GameObject.Find("PlayerCapsule").transform;
+        State = state.idle;
     }
     private void OnTriggerStay(Collider other)
     {
         ChaseState();
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        State = state.idle;
+    }
     public void ChaseState()
     {
+        State = state.chase;
         agent.SetDestination(playerObj.position);
     }
-
+    public enum state
+    {
+        idle,
+        patrol,
+        chase,
+        shoot
+    }
     // Update is called once per frame
     void Update()
     {
