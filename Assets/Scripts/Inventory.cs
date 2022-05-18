47b6195e3de63745555ded964f52c70233fe90aa
MrResistance <Paul1208Gush@Gmail.com>
2022-05-18 12:44:07 +0100

    Player can only switch to weapons they have picked up

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,6 +7,7 @@ public class Inventory : MonoBehaviour
     public Weapon currentWeapon;
     public Animator animator;
     public AnimatorOverrideController pistolAnim, shotgunAnim, minigunAnim, cannonAnim;
+    public bool hasKnife = false, hasPistol = false, hasShotgun = false, hasMinigun = false, hasCannon = false;
     public float fireRate = 0.5F;
     private float nextFire = 0.0F;
     // Start is called before the first frame update
@@ -28,18 +29,23 @@ public class Inventory : MonoBehaviour
                 animator.Play("Unarmed");
                 break;
         case 1:
+                if (hasKnife)
                 animator.SetTrigger("Ready");
                 break;
         case 2:
+                if (hasPistol)
                 animator.runtimeAnimatorController = pistolAnim;
                 break;
         case 3:
+                if (hasShotgun)
                 animator.runtimeAnimatorController = shotgunAnim;
                 break;
         case 4:
+                if (hasMinigun)
                 animator.runtimeAnimatorController = minigunAnim;
                 break;
         case 5:
+                if (hasCannon)
                 animator.runtimeAnimatorController = cannonAnim;
                 break;       
         }
