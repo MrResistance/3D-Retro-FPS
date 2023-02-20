18be302512824644d4d2579fac5ae22cef32d000
MrResistance <Paul1208Gush@Gmail.com>
2023-02-20 17:30:06 +0000

    Fixed enemy projectiles occasionally not hitting player

--- Assets/Scripts/ProjectileCollision.cs
+++ Assets/Scripts/ProjectileCollision.cs
@@ -16,6 +16,7 @@ public class ProjectileCollision : MonoBehaviour
         if (collision.collider.tag.Contains("Enemy") || collision.collider.tag.Contains("Player"))
         {
             //Do damage
+            Debug.Log("I HIT " + collision.gameObject.name + " Which has the tag: " + collision.gameObject.tag);
             health = collision.collider.GetComponent<Health>();
             if (health != null)
             {
